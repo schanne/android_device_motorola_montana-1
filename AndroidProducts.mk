17cc7617a17011cbfa7a1b3caade9b17545d4b8c
johnlaff <joaoaraxaiba@gmail.com>
2019-05-12 22:22:05 -0300

    montana: Havoc Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
  PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_montana.mk
+    $(LOCAL_DIR)/havoc_montana.mk
--- havoc_montana.mk
+++ havoc_montana.mk
@@ -22,22 +22,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from montana device
 $(call inherit-product, device/motorola/montana/device.mk)
 
-# Inherit some common PE stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Havoc stuff.
+$(call inherit-product, vendor/havoc/config/common.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Google Apps
-TARGET_MINIMAL_APPS := TRUE
-TARGET_GAPPS_ARCH := arm64
-# IS_GO_MODE := true
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := montana
-PRODUCT_NAME := aosp_montana
+PRODUCT_NAME := havoc_montana
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_MODEL := Moto G5S
